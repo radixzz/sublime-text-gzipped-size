378a05952bc05f0402eaf7e992ec528209f5ea45
Iván Juárez Núñez <ivan@blindgazer.com>
2016-02-05 20:58:05 -0600

    Renaming class and adding human readable format of bytes.

--- GZippedFileSize/main.py
+++ GZippedFileSize/main.py
@@ -2,15 +2,21 @@ import math
 import gzip
 import sublime, sublime_plugin
 
-class FileSizeStats(sublime_plugin.EventListener):
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Yi', suffix)
+
+class GZippedFileSize(sublime_plugin.EventListener):
 	def update(self, view):
 		r = sublime.Region(0, view.size())
 		text = bytes(view.substr(r), 'utf-8')
 		bytes_len = len(gzip.compress(text))
-		kb_len = math.ceil( bytes_len / 1024)
+		fmt_len = sizeof_fmt(bytes_len)
 		view.erase_status("gzippedSize")
-		view.set_status("gzippedSize", "gzipped: ~%s KB" % kb_len)
-
+		view.set_status("gzippedSize", "GZip %s" % fmt_len)
 
 	on_post_save_async = update
 	on_modified_async = update
